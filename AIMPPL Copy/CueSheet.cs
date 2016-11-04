5f3baa9fd983aa62a58cb6cb0213fe45264a0b17
ApexWeed <heli.freak.95@gmail.com>
2016-11-04 18:20:54 +1000

    Basic CUE sheet parsing.

---
+++ AIMPPL Copy/CueSheet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AIMPPL_Copy
+{
+    public class CueSheet
+    {
+        public string Filename;
+        public List<CueTrack> Tracks;
+
+        public CueSheet(string Filename)
+        {
+            this.Filename = Filename;
+            Tracks = new List<CueTrack>();
+
+            using (var r = new StreamReader(Filename))
+            {
+                var line = string.Empty;
+                var pos = 0L;
+
+                while (!r.EndOfStream)
+                {
+                    line = r.ReadLine();
+                    if (line.StartsWith("  TRACK") && line.EndsWith("AUDIO"))
+                    {
+                        var index = line.Split(' ')[3];
+                        var track = new CueTrack
+                        {
+                            ID = int.Parse(index)
+                        };
+
+                        line = r.ReadLine();
+                        while (!r.EndOfStream && line.StartsWith("    "))
+                        {
+                            var parts = line.Split(' ');
+                            switch (parts[4])
+                            {
+                                case "TITLE":
+                                    {
+                                        track.Title = line.Substring(11, line.Length - 12);
+                                        break;
+                                    }
+                                case "PERFORMER":
+                                    {
+                                        track.Performer = line.Substring(15, line.Length - 16);
+                                        break;
+                                    }
+                                case "SONGWRITER":
+                                    {
+                                        track.SongWriter = line.Substring(16, line.Length - 17);
+                                        break;
+                                    }
+                                case "INDEX":
+                                    {
+                                        track.Indexes.Add(parts[5], parts[6]);
+                                        break;
+                                    }
+                            }
+
+                            pos = Util.GetActualPosition(r);
+                            line = r.ReadLine();
+                        }
+
+                        // Rewind back to start of track.
+                        Util.SetActualPosition(r, pos);
+                        Tracks.Add(track);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AIMPPL Copy/CueTrack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIMPPL_Copy
+{
+    public class CueTrack
+    {
+        public int ID;
+        public string Title;
+        public string Performer;
+        public string SongWriter;
+        public Dictionary<string, string> Indexes;
+
+        public CueTrack()
+        {
+            Indexes = new Dictionary<string, string>();
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrWhiteSpace(Performer))
+            {
+                return Title;
+            }
+            else
+            {
+                return $"{Performer} - {Title}";
+            }
+        }
+    }
+}
